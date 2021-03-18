0831d65bac94717696931c2a31bb9211505fa904
bq <gOtQWGG4wZ>
2021-03-18 15:46:22 +0800

    HideYourKeys

--- src/main/java/com/github/makewheels/hideyourkeys/HideYourKeys.java
+++ src/main/java/com/github/makewheels/hideyourkeys/HideYourKeys.java
@@ -1,6 +1,10 @@
 package com.github.makewheels.hideyourkeys;
 
 public class HideYourKeys {
+    /**
+     * Spring Boot
+     * Support application.properties file only
+     */
     public static void hideKeysInSpringBoot() {
         SecretKeyUtil.overrideKeys();
     }
